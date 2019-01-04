eeca55b6d27b0973568ce9919d31572195ab466e
palewire <ben.welsh@gmail.com>
2019-01-03 21:01:37 -0800

    Upload

--- update.py
+++ update.py
@@ -1,13 +1,52 @@
+import os
+import gzip
+import json
+import boto3
 import requests
 import datetime
+from io import BytesIO
 
 
-def main():
+def main(file_name="tweets.json"):
+    """
+    Downloads all @realDonaldTrump tweets stored at trumptwitterarchive.com.
+
+    Uploads them as a single JSON to S3.
+    """
+    # Scrape
     base_url = 'http://www.trumptwitterarchive.com/data/realdonaldtrump/{}.json'
     years = range(2009, datetime.date.today().year)
     json_list = [requests.get(base_url.format(year)).json() for year in years]
     flat_list = [item for sublist in json_list for item in sublist]
     print(f"{len(flat_list)} tweets found")
+
+    # Write
+    buffer = BytesIO()
+    kwargs = dict(
+        filename=file_name,
+        mode='wb',
+        fileobj=buffer,
+        mtime=0
+    )
+    with gzip.GzipFile(**kwargs) as f:
+        f.write(json.dumps(flat_list).encode('utf-8'))
+    with open(file_name, 'wb') as outfile:
+        outfile.write(buffer.getvalue())
+    print(f"Wrote out gzipped JSON to {file_name}")
+
+    # Upload
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(os.getenv("AWS_BUCKET_NAME"))
+    bucket.upload_file(
+        file_name,
+        file_name,
+        ExtraArgs={
+            'ACL':'public-read',
+            'ContentEncoding': 'gzip',
+            'ContentType': "application/json"
+        }
+    )
+    print("Uploaded tweets.json to S3 bucket")
 
 
 if __name__ == '__main__':
