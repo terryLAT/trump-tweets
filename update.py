63035f0aa7bf8e67d231ae164e9aee51180fd93d
Castleman <tcastleman@la-dgt-54775.usa.tribune.com>
2019-03-06 12:54:12 -0800

    writing bogut info to file

--- update.py
+++ update.py
@@ -5,53 +5,49 @@ import boto3
 import requests
 import datetime
 from io import BytesIO
+from bs4 import BeautifulSoup
 
 
-def main(file_name="tweets.json"):
+def main(file_name='bogut.json'):
     """
     Downloads all @realDonaldTrump tweets stored at trumptwitterarchive.com.
 
     Uploads them as a single JSON to S3.
     """
-    # Scrape
-    base_url = 'http://www.trumptwitterarchive.com/data/realdonaldtrump/{}.json'
-    year_list = range(2009, datetime.date.today().year + 1)
-    json_list = []
-    for year in year_list:
-        r = requests.get(base_url.format(year))
-        data = r.json()
-        data.reverse()
-        json_list.append(data)
-    flat_list = [item for sublist in json_list for item in sublist]
-    print(f"{len(flat_list)} tweets found")
+    # Collect and parse page
+    page = requests.get('https://www.basketball-reference.com/players/b/bogutan01.html')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    per_game_stats = soup.find(id='per_game.2018')
+    flat_list = [item for sublist in per_game_stats for item in sublist]
+    games_played = flat_list[5]
+    games_started = flat_list[6]
+    avg_mins = flat_list[7]
+
+    data = {}
+    data['games_played'] = games_played
+    data['games_started'] = games_started
+    data['avg_mins'] = avg_mins
+    #json_data = json.dumps(data)
 
     # Write
-    buffer = BytesIO()
-    kwargs = dict(
-        filename=file_name,
-        mode='wb',
-        fileobj=buffer,
-        mtime=0
-    )
-    with gzip.GzipFile(**kwargs) as f:
-        f.write(json.dumps(flat_list).encode('utf-8'))
-    with open(file_name, 'wb') as outfile:
-        outfile.write(buffer.getvalue())
-    print(f"Wrote out gzipped JSON to {file_name}")
+    with open(file_name, 'w') as outfile:
+        json.dump(data, outfile)
+
+    print(f"Wrote out gzipped JSON file to {file_name}")
 
     # Upload
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(os.getenv("AWS_BUCKET_NAME"))
-    bucket.upload_file(
-        file_name,
-        file_name,
-        ExtraArgs={
-            'ACL':'public-read',
-            'ContentEncoding': 'gzip',
-            'ContentType': "application/json"
-        }
-    )
-    print("Uploaded tweets.json to S3 bucket")
+    # s3 = boto3.resource('s3')
+    # bucket = s3.Bucket(os.getenv("AWS_BUCKET_NAME"))
+    # bucket.upload_file(
+    #     file_name,
+    #     file_name,
+    #     ExtraArgs={
+    #         'ACL':'public-read',
+    #         'ContentEncoding': 'gzip',
+    #         'ContentType': "application/json"
+    #     }
+    # )
+    # print("Uploaded tweets.json to S3 bucket")
 
 
 if __name__ == '__main__':
